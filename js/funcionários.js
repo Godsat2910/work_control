04a3f49f2d230b7e29f5b85ec3c41fad176a807a
Godsat2910 <igorcrzsatiro@gmail.com>
2021-12-03 18:26:17 -0300

    adm e funcionarios

---
+++ js/funcionários.js
@@ -0,0 +1,7 @@
+let horarios = [35, 48, 120, 128, 80, 90, 45, 72]
+
+console.log(horarios)
+
+for(let pos=0; pos < horarios.length; pos++) {
+    console.log('O funcionário ${pos} tem ${horarios[pos]} horas de trabalho')
+}
